a04219804127da8171b3ade5a6bcf51ddfacd2bd
StiflerPasha <dvoynospavel@gmail.com>
2019-09-18 13:20:31 +0300

    use flexbox style

--- App.js
+++ App.js
@@ -5,17 +5,47 @@ const App = () => {
 
    return (
 	  <View style={ styles.container }>
-		 <Text>Hello world</Text>
+		 <View style={ styles.red }>
+			<Text>1</Text>
+		 </View>
+		 <View style={ styles.blue }>
+			<Text>2</Text>
+		 </View>
+		 <View style={ styles.green }>
+			<Text>3</Text>
+		 </View>
 	  </View>
    );
 };
 
 const styles = StyleSheet.create({
    container: {
-	  flex: 1,
-	  backgroundColor: '#fff',
-	  alignItems: 'center',
+	  padding: 50,
+	  flexDirection: 'row',
+	  width: '80%',
+	  height: 300,
+	  justifyContent: 'space-around',
+	  alignItems: 'stretch',
+	  borderWidth: 2,
+	  borderColor: 'red'
+   },
+   red: {
+	  backgroundColor: 'red',
 	  justifyContent: 'center',
+	  alignItems: 'center',
+	  flex: 1,
+   },
+   blue: {
+	  backgroundColor: 'blue',
+	  justifyContent: 'center',
+	  alignItems: 'center',
+	  flex: 1,
+   },
+   green: {
+	  backgroundColor: 'green',
+	  justifyContent: 'center',
+	  alignItems: 'center',
+	  flex: 1,
    },
 });
 
