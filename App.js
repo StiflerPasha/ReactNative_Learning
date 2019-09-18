20d9f586c9198aa8c02e26236daaaec9fa8c74a2
StiflerPasha <dvoynospavel@gmail.com>
2019-09-18 17:34:07 +0300

    split by components

--- App.js
+++ App.js
@@ -1,29 +1,46 @@
-import React from 'react';
-import {
-   StyleSheet,
-   Text,
-   View,
-   TextInput,
-   Button,
-}            from 'react-native';
+import React, { useState }            from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+import GoalItem                       from './components/GoalItem';
+import GoalInput                      from './components/GoalInput';
 
 const App = () => {
+   const [courseGoals, setCourseGoals] = useState([]);
+
+   const addGoalHandler = (goalTitle) => {
+	  if (goalTitle.trim()) {
+		 const newGoal = {
+			id: Math.random().toString(),
+			value: goalTitle,
+		 };
+		 setCourseGoals(currentGoals => [...currentGoals, newGoal]);
+	  }
+	  //setEnteredGoal('');
+   };
 
    return (
-	  <View style={ { padding: 50 } }>
-		 <View style={ { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' } }>
-			<TextInput
-			   placeholder="Course Goal"
-			   style={ {width: '80%', borderBottomColor: 'black', borderWidth: 1, padding: 10 } } />
-			<Button title="ADD" />
-		 </View>
-		 <View>
-
-		 </View>
+	  <View style={ styles.screen }>
+		 <GoalInput onAddGoal={ addGoalHandler } />
+		 <FlatList
+			//change 'KEY to smth else e.g. ID'
+			keyExtractor={ (item, index) => item.id }
+			data={ courseGoals }
+			renderItem={ itemData => (
+			   <GoalItem title={ itemData.item.value } />
+			) } />
 	  </View>
    );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+   screen: {
+	  padding: 50,
+   },
+
+   /*goalsList: {
+	  height: '50%',
+	  borderWidth: 2,
+	  borderColor: 'red',
+   },*/
+});
 
 export default App;
