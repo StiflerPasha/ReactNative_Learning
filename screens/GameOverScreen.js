0d25eb8c9b2c2f1ab0687a3f16a883bc23b22d66
StiflerPasha <dvoynospavel@gmail.com>
2019-09-22 19:03:17 +0300

    change some style

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,14 +1,28 @@
-import React                              from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React                                        from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Card                                         from '../components/Card';
+import NumberContainer                              from '../components/NumberContainer';
+import Colors                                       from '../constants/colors';
 
 
-const GameOverScreen = (props) => {
+const GameOverScreen = ({ roundsNumber, userNumber, onRestart }) => {
+
    return (
 	  <View style={ styles.screen }>
-		 <Text>The Game is Over!</Text>
-		 <Text>Number of rounds: { props.roundsNumber }</Text>
-		 <Text>Number was: { props.userNumber }</Text>
-		 <Button onPress={ props.onRestart } title="NEW GAME" />
+		 <Text style={ styles.gameOver }>Game Over!</Text>
+		 <TouchableOpacity onPress={ onRestart } style={ styles.button }>
+			<Text>NEW GAME</Text>
+		 </TouchableOpacity>
+		 <View style={ styles.resultsContainer }>
+			<Card style={ styles.results }>
+			   <Text>Number of rounds: </Text>
+			   <NumberContainer style={ styles.number }>{ roundsNumber }</NumberContainer>
+			</Card>
+			<Card style={ styles.results }>
+			   <Text>Number was: </Text>
+			   <NumberContainer style={ styles.number }>{ userNumber }</NumberContainer>
+			</Card>
+		 </View>
 	  </View>
    );
 };
@@ -16,10 +30,37 @@ const GameOverScreen = (props) => {
 const styles = StyleSheet.create({
    screen: {
 	  flex: 1,
+	  justifyContent: 'space-around',
 	  alignItems: 'center',
+   },
+   gameOver: {
+	  color: 'red',
+	  fontSize: 42,
+   },
+   resultsContainer: {
+	  flexDirection: 'row',
+	  width: '100%',
+	  justifyContent: 'space-around',
+
+   },
+   results: {
+	  padding: 5,
+	  width: '35%',
 	  justifyContent: 'center',
+	  alignItems: 'center',
+   },
+   button: {
+	  justifyContent: 'center',
+	  alignItems: 'center',
+	  height: 150,
+	  width: 150,  //The Width must be the same as the height
+	  borderRadius: 300, //Then Make the Border Radius twice the size of width or Height
+	  backgroundColor: Colors.accent,
+   },
+   number: {
+	  width: '50%',
+	  textAlign: 'center',
    },
 });
 
-//TODO: Style this screen
 export default GameOverScreen;
